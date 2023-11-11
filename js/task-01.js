b4b9ff30caa2393c3bd18b16953792ae4fb5ebbb
RAGE1712 <137414196+RAGE1712@users.noreply.github.com>
2023-11-11 22:45:12 +0200

    task 1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const itemCategories = document.querySelectorAll(".item")
+console.log(`Number of categories: ${itemCategories.length}`)
+
+itemCategories.forEach((category,i) => {
+    const header = category.querySelector("h2")
+    console.log("");
+    console.dir(`Category: ${header.textContent}`)
+    const element = category.querySelectorAll("li")
+    console.dir(`Elements: ${element.length}`)
+})
